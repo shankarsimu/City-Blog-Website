ef02e8ebe91688ecffd13f0c72ff2cc323c42139
shankar <shankarnarayan791@gmail.com>
2019-12-12 14:51:28 +0530

    website

---
+++ dbb/new.js
@@ -0,0 +1,16 @@
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5bc7172708387933e5bba037/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+ 
